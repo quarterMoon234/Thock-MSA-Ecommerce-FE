437a8c91c54343817d55b5140e264f3ca2a1edb7
Eom Sanghyun <quarterMoon234@gmail.com>
2026-03-31 16:31:57 +0900

    frontend save commit

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+import { AuthProvider } from './contexts/AuthContext'
+import { CartProvider } from './contexts/CartContext'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <AuthProvider>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </AuthProvider>
+  </StrictMode>,
+)
